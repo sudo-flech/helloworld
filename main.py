a8ed7838aa73df0150b2beca8d5fc6ce3786ca64
Strelka <85939235+sudo-flech@users.noreply.github.com>
2021-12-02 14:59:46 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+print("hi, welcome")
+
+x = int(input("put a number"))
+
+if x<0:
+  print("the number is positive")
+  
+elif x>0:
+  print("the number is negative")
+  
+ else:
+  print("put a valid number")
